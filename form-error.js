23d2e8eccc0d775c99a497362794b1dd49655a79
Zacky Ma <zacky@marchbox.com>
2023-12-30 00:21:30 -0800

    remove pattern feature for now

--- form-error.js
+++ form-error.js
@@ -1,5 +1,3 @@
-import FormErrorRegistry from './registry.js';
-
 // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/elements#value
 const VALID_CONTROL_ELEMENT_QUERY = [
   'fieldset',
@@ -10,7 +8,7 @@ const VALID_CONTROL_ELEMENT_QUERY = [
 ].join(',');
 
 // kabab-case to cammelCase
-function normalizeValidity(value) {
+function k2c(value) {
   if (!value.includes('-')) {
     return value;
   }
@@ -20,11 +18,8 @@ function normalizeValidity(value) {
 
 export default class FormError extends HTMLElement {
   #htmlFor = '';
-  #pattern = '';
   #validity = '';
 
-  #registry;
-  #inputEl;
   #msgNode;
 
   static {
@@ -33,9 +28,16 @@ export default class FormError extends HTMLElement {
 
   static observedAttributes = [
     'for',
-    'pattern',
     'validity',
   ];
+
+  get control() {
+    return document.getElementById(this.#htmlFor);
+  }
+
+  get form() {
+    return this.control?.form;
+  }
 
   get htmlFor() {
     return this.#htmlFor;
@@ -45,28 +47,6 @@ export default class FormError extends HTMLElement {
     if (this.#htmlFor !== value) {
       this.setAttribute('for', value);
     }
-  }
-
-  get pattern() {
-    return this.#pattern;
-  }
-
-  set pattern(value) {
-    if (this.#pattern !== value) {
-      this.setAttribute('pattern', value);
-    }
-  }
-
-  get patternAsRegExp() {
-    try {
-      return new RegExp(this.#pattern);
-    } catch {}
-
-    return null;
-  }
-
-  set patternAsRegExp(value) {
-    this.pattern = value instanceof RegExp ? value.source : value.toString();
   }
 
   get validity() {
@@ -79,38 +59,6 @@ export default class FormError extends HTMLElement {
     }
   }
 
-  get control() {
-    return document.getElementById(this.#htmlFor);
-  }
-
-  get form() {
-    return this.control?.form;
-  }
-
-  get message() {
-    return this.#msgNode?.textContent || '';
-  }
-
-  set message(value) {
-    if (this.#msgNode?.nodeType !== Node.TEXT_NODE) {
-      this.#msgNode = document.createTextNode('');
-      this.append(this.#msgNode);
-    }
-
-    this.#msgNode.textContent = value.toString();
-  }
-
-  constructor() {
-    super();
-
-    if (!window['__FORM_ERROR_REGISTRY__']) {
-      window['__FORM_ERROR_REGISTRY__'] = new FormErrorRegistry();
-    }
-
-    this.#registry = window['__FORM_ERROR_REGISTRY__'];
-    this.#registry.register(this);
-  }
-
   connectedCallback() {
     this.#setUpProps();
 
@@ -120,7 +68,26 @@ export default class FormError extends HTMLElement {
 
     this.control.addEventListener('invalid', evt => {
       evt.preventDefault();
-      this.#validate();
+
+      if (this.control.validity.patternMismatch && this.control.title) {
+        this.#setMessage(this.control.title);
+      } else {
+        const customMessage = this.querySelector('template')?.content
+            .cloneNode(true).textContent;
+        this.#setMessage(customMessage ?? this.control.validationMessage);
+      }
+    });
+
+    this.control.addEventListener('blur', () => {
+      if (this.control.validity.valid) {
+        this.#setMessage('');
+      }
+    });
+
+    this.form.addEventListener('submit', () => {
+      if (this.control.validity.valid) {
+        this.#setMessage('');
+      }
     });
   }
 
@@ -129,48 +96,27 @@ export default class FormError extends HTMLElement {
       case 'for':
         this.#htmlFor = this.getAttribute('for');
         break;
-      case 'pattern':
-        this.#pattern = this.getAttribute('pattern');
-        break;
       case 'validity':
         this.#validity = this.getAttribute('validity');
         break;
     }
   }
 
-  #validate() {
-    if (this.control.validity.valid) {
-      this.message = '';
-    } else if (this.#validity) {
-      if (!this.control.validity[normalizeValidity(this.validity)]) {
-        return;
-      }
-
-      const template = this.querySelector('template');
-
-      if (!template) {
-        this.message = this.control.validationMessage;
-        return;
-      }
-
-      this.message = template.content.cloneNode(true).textContent;
-    } else if (this.#pattern) {
-      // TODO
-    } else {
-      this.message = this.control.validationMessage;
+  #setMessage(value) {
+    if (this.#msgNode?.nodeType !== Node.TEXT_NODE) {
+      this.#msgNode = document.createTextNode('');
+      this.append(this.#msgNode);
     }
+
+    this.#msgNode.textContent = value.toString();
   }
 
   #setUpProps() {
     const initialHtmlFor = this.getAttribute('for');
-    const initialPattern = this.getAttribute('pattern');
     const initialValidity = this.getAttribute('validity');
 
     if (initialHtmlFor) {
       this.#htmlFor = initialHtmlFor;
-    }
-    if (initialPattern) {
-      this.#pattern = initialPattern;
     }
     if (initialValidity) {
       this.#validity = initialValidity;
--- registry.js
+++
@@ -1,7 +0,0 @@
-export default class FormErrorRegistry {
-  #elements = new Set();
-
-  register(element) {
-    this.#elements.add(element);
-  }
-}
