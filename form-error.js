7ab1249c93d8ffd3d73f8a1e9cb2fc4cb5675945
Zacky Ma <zacky@marchbox.com>
2025-06-16 19:12:28 -0700

    update demo

--- form-error.js
+++ form-error.js
@@ -6,21 +6,20 @@ const VALID_CONTROL_ELEMENT_QUERY = [
 	"textarea",
 ].join(",");
 
-// kabab-case to cammelCase
-function k2c(value) {
-	if (!value.includes("-")) {
-		return value;
-	}
-
-	return value.toLowerCase().replace(/-(\w)/g, (_, l) => l.toUpperCase());
-}
-
 export default class FormError extends HTMLElement {
+	/** @type {string} */
 	#htmlFor = "";
+
+	/** @type {string} */
 	#validity = "";
 
-	#msgNode;
+	/** @type {DocumentFragment} */
+	#content = new DocumentFragment();
+
+	/** @type {AbortController} */
 	#disconnectedAbortController = new AbortController();
+
+	/** @type {AbortController} */
 	#connectedControlAbortController = new AbortController();
 
 	static {
@@ -30,28 +29,39 @@ export default class FormError extends HTMLElement {
 
 	static observedAttributes = ["for", "validity"];
 
+	/** @return {DocumentFragment} */
+	get content() {
+		return this.#content;
+	}
+
+	/** @return {HTMLInputElement | HTMLFieldSetElement | HTMLSelectElement | HTMLTextAreaElement | null} */
 	get control() {
 		return document.getElementById(this.#htmlFor);
 	}
 
+	/** @return {HTMLFormElement | null} */
 	get form() {
 		return this.control?.form;
 	}
 
+	/** @return {string} */
 	get htmlFor() {
-		return this.#htmlFor;
+		return this.#htmlFor ?? "";
 	}
 
+	/** @param {string} value */
 	set htmlFor(value) {
 		if (this.#htmlFor !== value) {
 			this.setAttribute("for", value);
 		}
 	}
 
+	/** @return {string} */
 	get validity() {
-		return this.#validity;
+		return this.#validity ?? "";
 	}
 
+	/** @param {string} value */
 	set validity(value) {
 		if (this.#validity !== value) {
 			this.setAttribute("validity", value);
@@ -65,6 +75,17 @@ export default class FormError extends HTMLElement {
 			return;
 		}
 
+		if (
+			this.control &&
+			!this.querySelector("template") &&
+			this.innerHTML.trim() !== ""
+		) {
+			this.control.setCustomValidity(this.textContent.trim());
+			this.#show();
+			return;
+		}
+
+		this.append(this.#content);
 		this.#connectToControl();
 	}
 
@@ -89,23 +110,19 @@ export default class FormError extends HTMLElement {
 		}
 	}
 
-	#show(value) {
-		if (this.#msgNode?.nodeType !== Node.TEXT_NODE) {
-			this.#msgNode = document.createTextNode("");
-			this.append(this.#msgNode);
-		}
-
-		this.#msgNode.textContent = value.toString();
-
-		if (value !== "") {
-			this.dispatchEvent(new CustomEvent("errorshow", { bubbles: true }));
+	#show() {
+		const msg = this.#getMessage();
+		if (msg) {
+			this.#content.appendChild(msg);
+			this.dispatchEvent(new Event("errorshow", { bubbles: true }));
+		} else {
+			this.#clear();
 		}
 	}
 
 	#clear() {
-		this.#show("");
-
-		this.dispatchEvent(new CustomEvent("errorclear", { bubbles: true }));
+		this.#content.replaceChildren();
+		this.dispatchEvent(new Event("errorhide", { bubbles: true }));
 	}
 
 	#maybeClear() {
@@ -114,11 +131,58 @@ export default class FormError extends HTMLElement {
 		}
 	}
 
+	/** @return {Node | string | null} */
+	#getMessage() {
+		const isEmpty = this.innerHTML.trim() === "";
+
+		if (
+			(!this.#validity && !this.control.validity.valid) ||
+			(this.#validity && isEmpty && this.#hasMatchingValidity())
+		) {
+			if (this.control.validity.patternMismatch && this.control.title) {
+				return this.control.title;
+			}
+			return this.control.validationMessage;
+		}
+
+		const tpl = this.querySelector("template");
+
+		if (tpl) {
+			return tpl.content.cloneNode(true);
+		}
+
+		return isEmpty ? null : this.innerHTML;
+	}
+
 	#hasMatchingValidity() {
-		return (
-			!this.#validity ||
-			(this.#validity && this.control.validity[k2c(this.#validity)])
-		);
+		if (!this.control) {
+			return false;
+		}
+
+		switch (this.#validity?.toLowerCase()) {
+			case "badinput":
+				return this.control.validity.badInput;
+			case "customerror":
+				return this.control.validity.customError;
+			case "patternmismatch":
+				return this.control.validity.patternMismatch;
+			case "rangeoverflow":
+				return this.control.validity.rangeOverflow;
+			case "rangeunderflow":
+				return this.control.validity.rangeUnderflow;
+			case "stepmismatch":
+				return this.control.validity.stepMismatch;
+			case "toolong":
+				return this.control.validity.tooLong;
+			case "tooshort":
+				return this.control.validity.tooShort;
+			case "typemismatch":
+				return this.control.validity.typeMismatch;
+			case "valuemissing":
+				return this.control.validity.valueMissing;
+			default:
+				return false;
+		}
 	}
 
 	#setUpProps() {
@@ -155,17 +219,7 @@ export default class FormError extends HTMLElement {
 					return;
 				}
 
-				if (
-					this.control.validity.patternMismatch &&
-					this.control.title &&
-					!this.querySelector("template")
-				) {
-					this.#show(this.control.title);
-				} else {
-					const customMessage =
-						this.querySelector("template")?.content.cloneNode(true).textContent;
-					this.#show(customMessage ?? this.control.validationMessage);
-				}
+				this.#show();
 			},
 			{ signal },
 		);
